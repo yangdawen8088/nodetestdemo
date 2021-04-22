0ca2651fff755dc96985cdeb5b2f31a4a41f87e1
Wen Yang <v-wenyang@microsoft.com>
2021-04-22 18:23:06 +0800

    2-4课程技术提交

--- 2-2/src/app.js
+++ 2-2/src/app.js
@@ -1,34 +1,46 @@
 const express = require('express');
 const app = express();
 //1.通过请求的方法类型：get/post/put/delete
-app.get('/demo', (req, res) => {
-    //req：请求对象
-    //res：服务器响应对象
+// app.get('/demo', (req, res) => {
+//     //req：请求对象
+//     //res：服务器响应对象
+//     res.json({
+//         message: "你好！你通过get的请求"
+//     });
+// })
+// app.post('/test', (req, res) => {
+//     //req：请求对象
+//     //res：服务器响应对象
+//     res.json({
+//         message: "你好！你通过post的请求"
+//     });
+// })
+// //2.通过URL
+// app.get('/user/byname', (req, res) => {
+//     let { name } = req.query;
+//     res.json({
+//         name
+//     });
+// })
+// app.get('/user/byid', (req, res) => {
+//     let { id } = req.query;
+//     res.json({
+//         id
+//     });
+// })
+// app.all('/demo', (req, res) => {
+//     res.json({
+//         message: "这是一个演示",
+//         method: req.method
+//     });
+// })
+app.all('*', (req, res) => {
     res.json({
-        message: "你好！你通过get的请求"
+        message: "这是一个演示",
+        method: req.method,
+        url:req.path
     });
 })
-app.post('/test', (req, res) => {
-    //req：请求对象
-    //res：服务器响应对象
-    res.json({
-        message: "你好！你通过post的请求"
-    });
-})
-//2.通过URL
-app.get('/user/byname', (req, res) => {
-    let { name } = req.query;
-    res.json({
-        name
-    });
-})
-app.get('/user/byid', (req, res) => {
-    let { id } = req.query;
-    res.json({
-        id
-    });
-})
-
 
 
 app.listen(8088, '127.0.0.1', () => {
